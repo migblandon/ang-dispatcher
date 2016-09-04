f0a907a8c57c655648c82038062d805c2d75dd32
Miguel Guevara <mguev023@fiu.edu>
2016-09-04 18:56:36 -0400

    Switch the directive controller to a more settle service

--- ang-dispatcher.js
+++ ang-dispatcher.js
@@ -6,73 +6,60 @@
  */
 (function(window, angular) {'use strict';
 
-angular.module('angDispatcher', []).
-	directive('angControl', function(){
+angular.module('angDispatcher', [])
+	.service('$spareController', [function(){
+		var dest = [];
+		var element = [];
+		var signal = function(receiver){
+			receiver.html(element.html());
+		};
+		this.addElement = function(el, name){
+			if(dest.length > 0)
+				angular.forEach(dest, function(receiver){
+					if(receiver.name == name)
+						receiver.item.html(el.html());
+					else if(!receiver.name)
+						receiver.item.html(el.html());
+				});
+			element.push({item: el, name: name});
+		};
+		this.holder = function(receiver, name){
+			if(name){
+				var r = element.find(function(item){
+					return item.name == name;
+				});
+				if(r) receiver.html(r.item.html());
+				else dest.push({item: receiver, name: name});
+			}
+			else if(element.length > 0){
+				receiver.html(element[0].item.html());
+			}
+			else {
+				dest.push({item: receiver, name: name});
+			}
+		};	
+}])
+.directive('angCentral', ['$spareController', function($spareController){
 	return {
 		restrict: 'EA',
-		priority: 400,
 		transclude: true,
-		scope: {},
-		controller: ['$scope', function viewController($scope){
-			var dest = [];
-			var element = [];
-			var signal = function(receiver){
-				receiver.html(element.html());
-			};
-			this.addElement = function(el, name){
-				if(dest.length > 0)
-					angular.forEach(dest, function(receiver){
-						if(receiver.name == name)
-							receiver.item.html(el.html());
-						else if(!receiver.name)
-							receiver.item.html(el.html());
-					});
-				element.push({item: el, name: name});
-			};
-			this.holder = function(receiver, name){
-				if(name){
-					var r = element.find(function(item){
-						return item.name == name;
-					});
-					if(r) receiver.html(r.item.html());
-					else dest.push({item: receiver, name: name});
-				}
-				else if(element.length > 0){
-					receiver.html(element[0].item.html());
-				}
-				else {
-					dest.push({item: receiver, name: name});
-				}
-			};
-			
-		}],
-		template: '<div ng-transclude></div>'
-	};
-		
-})
-.directive('angCentral', function(){
-	return {
-		require: '^^angControl',
-		restrict: 'EA',
-		transclude: true,
-		link: function(scope, element, attr, control){
-			control.addElement(element, attr.angCentral);
+		link: function(scope, element, attr){
+			$spareController.addElement(element, attr.angCentral);
 		},
 		template: '<div ng-transclude></div>'
 	};
 		
-})
-.directive('angReceiver', function(){
+}])
+.directive('angReceiver', ['$spareController', function($spareController){
 	return {
-		require: '^^angControl',
 		restrict: 'EA',
 		transclude: true,
-		link: function(scope, element, attr, control){
-			control.holder(element, attr.angReceiver);
+		link: function(scope, element, attr){
+			$spareController.holder(element, attr.angReceiver);
 		}
 	};
 		
-});
+}]);
 
 
 	
