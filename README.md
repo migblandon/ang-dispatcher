fd0e2ce461556fe8f4da2937ff88c2697c142741
Miguel Guevara <mguev023@fiu.edu>
2016-09-04 20:27:26 -0400

    Added comments to the ang-dispatcher file and updated README to be prsentable

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-titos-bootwrap:
-new inovated bootstap created by Miguel Guevara
+If you ever need to copy code from your html, then "angDispatcher" was designed for that.
+Take any node in yout tree and clone it to any other location in your tree. Even take an array of nodes
+and added in in multiple places. 
+You use two directives to handle the nodes and that is all there is. 
+
+DIRECTIVES:
+
+central => 'ang-central' => '<div ang-central="example"></div'
+#central will still print out your node but it will clone it to be used in any other location.
+
+receiver => 'ang-receiver' => '<div ang-receiver="example"></div'
+#receiver will receive a clone and use it has the original node.
+
+Only add 'angDispatcher' to your project and you are all set to start using it.
+Add as many centrals you need and have as many receivers as you like.
+
+Created by: Miguel Guevara
--- ang-dispatcher.js
+++ ang-dispatcher.js
@@ -1,18 +1,49 @@
 /**
- * @license Unknown
- * (c) 2016 [company-name] [website]
+ * @license Open source
+ * (c) 2016  github.com/migblandon/ang-dispatcher
  * License: MIT
  * Created by: Miguel Guevara
  */
 (function(window, angular) {'use strict';
-
+/**
+ * @doc module
+ * @name angDispatcher
+ * @description
+ *
+ * =>angDispatcher<=
+ *
+ * The 'Dispatcher' makes it easy to reuse modules inside your DOM tree
+ *
+ * Add the module to your project and you will be ready to use it's directives
+ * 
+ */
 angular.module('angDispatcher', [])
-	.service('$spareController', [function(){
+	 /**
+   * @doc service
+   * @name $dispatcher
+   * @description
+   * 
+   * Dispatcher controller is used by the application to control who is who and where things are going.
+   * 
+   * 
+   * 
+   * The $dispatcher service is NOT used outside of this scope. It is meant to live internally/ 
+   * */
+	.service('$dispatcher', [function(){
 		var dest = [];
 		var element = [];
-		var signal = function(receiver){
-			receiver.html(element.html());
-		};
+		 /**
+		 * @doc method
+		 * @name $dispatcher#addElement
+		 * @description
+		 *
+		 * 'addElement' first broadcast to any receiver waiting for the element. then,
+		 *  it holds the html node for upcoming receivers.
+		 * 
+		 * @param {html node} el Node that will be clone o be reused.
+		 * @param {string} name Alias of the html node.
+		 * 
+		 **/
 		this.addElement = function(el, name){
 			if(dest.length > 0)
 				angular.forEach(dest, function(receiver){
@@ -23,6 +54,17 @@ angular.module('angDispatcher', [])
 				});
 			element.push({item: el, name: name});
 		};
+		/**
+		 * @doc method
+		 * @name $dispatcher#holder
+		 * @description
+		 *
+		 * 'holder' checks if there are any transmitters in place and saves the html node
+		 * 
+		 * @param {html node} receiver Element in which another node will be injected.
+		 * @param {string} name Alias of the html node.
+		 * 
+		 **/
 		this.holder = function(receiver, name){
 			if(name){
 				var r = element.find(function(item){
@@ -39,28 +81,61 @@ angular.module('angDispatcher', [])
 			}
 		};	
 }])
-.directive('angCentral', ['$spareController', function($spareController){
+/**
+ * @ngdoc directive
+ * @name angCentral
+ *
+ * @description
+ * Clones the html node and passes it to other nodes wherever they are needed.
+ * 
+ * <div ang-central="example"></div> 
+ * ------->or<--------- 
+ * <div ang-central=""</div> 
+ * ------->or<--------- 
+ * <div ang-central></div
+ * 
+ * Id the node with a name (string), or without a name.
+ * 
+ * ang-central' can be anywhere in your code, any 'ang-receiver' will identify
+ * which node to add according to the name or if it has no name, it adds the 
+ * first node that was added to central.
+ */
+.directive('angCentral', ['$dispatcher', function($dispatcher){
 	return {
 		restrict: 'EA',
 		transclude: true,
 		link: function(scope, element, attr){
-			$spareController.addElement(element, attr.angCentral);
+			$dispatcher.addElement(element, attr.angCentral);
 		},
 		template: '<div ng-transclude></div>'
 	};
 		
 }])
-.directive('angReceiver', ['$spareController', function($spareController){
+/**
+ * @ngdoc directive
+ * @name angReceiver
+ *
+ * @description
+ * adds a cloned node to the appropiate html node
+ * 
+ * <div ang-receiver="example"></div> 
+ * ------->or<---------  
+ * <div ang-receiver=""></div> 
+ * ------->or<---------  
+ * <div ang-receiver></div>
+ * 
+ * receives a node cloned by 'ang-central'.
+ * if the node has a name, it will look for that node. Otherwise, 
+ * it will add the first node added from top to bottom of the DOM.
+ */
+.directive('angReceiver', ['$dispatcher', function($dispatcher){
 	return {
 		restrict: 'EA',
 		transclude: true,
 		link: function(scope, element, attr){
-			$spareController.holder(element, attr.angReceiver);
+			$dispatcher.holder(element, attr.angReceiver);
 		}
 	};
 		
 }]);
-
-
-	
 })(window, window.angular);
